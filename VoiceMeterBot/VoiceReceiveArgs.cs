051d2bb11f7919cbed537aae6e31eaf9d70aa952
Stropheum <stropheum060@gmail.com>
2024-10-17 02:26:51 -0400

    added voicereceiveargs

---
+++ VoiceMeterBot/VoiceReceiveArgs.cs
@@ -0,0 +1,15 @@
+﻿using NetCord;
+using NetCord.Gateway.Voice;
+using Newtonsoft.Json;
+
+namespace VoiceMeterBot;
+
+[Serializable]
+public class VoiceActivityEventArgs
+{
+    [JsonProperty] public DateTime TimeStamp { get; set; }
+    [JsonProperty] public User? User { get; set; }
+    [JsonProperty] public uint Ssrc { get; set; }
+    [JsonProperty] public ulong UserId { get; set; }
+    [JsonProperty] public byte[]? Frame { get; set; }
+}
