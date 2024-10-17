8cc9980e72587e6b47699bb94e70f4ee1a924344
Stropheum <stropheum060@gmail.com>
2024-10-17 02:26:14 -0400

    removed secrets

---
+++ VoiceMeterBot/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VoiceMeterBot;
+
+internal static class Program
+{
+    private static async Task Main()
+    {
+        var bot = new Bot(BotToken, GuildId, VoiceChannelId);
+        await bot.Connect();
+        await Task.Delay(-1);
+    }
+}
