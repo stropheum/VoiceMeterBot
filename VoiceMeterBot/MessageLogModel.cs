d81be8dea75a5daaf3a04dc51a455c61a4c85f21
Stropheum <stropheum060@gmail.com>
2024-10-17 01:53:25 -0400

    added messagelogmodel

---
+++ VoiceMeterBot/MessageLogModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace VoiceMeterBot;
+
+[Serializable]
+public class MessageLogModel
+{
+    [JsonProperty] public string? Name { get; set; }
+    [JsonProperty] public string Payload { get; set; }
+}
